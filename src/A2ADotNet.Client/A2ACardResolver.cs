08b0a92d861b969361fda2b006c52330ed4c19ab
Marius Bughiu <marius.bughiu@gmail.com>
2025-04-13 23:02:32 +0300

    fix GetAgentCard

--- src/A2ADotNet.Client/A2ACardResolver.cs
+++ src/A2ADotNet.Client/A2ACardResolver.cs
@@ -2,22 +2,21 @@
 
 using System.Net.Http;
 using System.Text.Json;
-using A2ADotNet;
 
 public class A2ACardResolver(string baseUrl, string agentCardPath = "/.well-known/agent.json")
 {
     private readonly string _baseUrl = baseUrl.TrimEnd('/');
     private readonly string _agentCardPath = agentCardPath.TrimStart('/');
 
-    public AgentCard GetAgentCard()
+    public async Task<AgentCard> GetAgentCard()
     {
         using var httpClient = new HttpClient();
-        var response = httpClient.GetAsync($"{_baseUrl}/{_agentCardPath}").Result;
+        var response = await httpClient.GetAsync($"{_baseUrl}/{_agentCardPath}");
         response.EnsureSuccessStatusCode();
 
         try
         {
-            var json = response.Content.ReadAsStringAsync().Result;
+            var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<AgentCard>(json)
                    ?? throw new A2AClientJSONError($"Failed to deserialize {nameof(AgentCard)}.");
         }
